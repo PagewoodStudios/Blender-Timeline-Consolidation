71a764cb7babedcd83c4b7f940ee1e8292f4a5e9
Pagewood Studios <32869231+PagewoodStudios@users.noreply.github.com>
2018-06-29 12:47:35 +1000

    Update and rename Blender Timeline Consolidation.py to Button Docking.py

---
+++ Button Docking.py
@@ -0,0 +1,15 @@
+'''
+Synopsis:
+No code artist friendly UI edit mode: 
+ - Click and drag button around  and dock
+ - Click and drag 
+      - Ui elements and doc where you want
+      - Create custom menus items
+      - Ability for custom menus to float in either
+             - Relevant to the viwer in blender 
+             - Float across Blenders viewers at an OS level
+             - This needs to be optional
+
+
+
+'''
